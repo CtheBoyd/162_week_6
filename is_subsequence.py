fa4b1b16b93571e8302f81cf1e70c51cef127a71
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-18 20:27:56 -0700

    update

--- is_subsequence.py
+++ is_subsequence.py
@@ -1,5 +1,21 @@
 # Author: Christopher Boyd
 # GitHub username: CtheBoyd
 # Date: 7/18/2022
-# Description: Recursive function that takes as its parameter a list of numbers and returns the maximum value in the list.
+# Description:  Recursive function that takes two string parameters and returns True if the first string is a
+# subsequence of the second string, but returns False otherwise.
 #
+
+def is_subsequence(a1, b1):
+    """Recursive function that takes two string parameters and returns True if the first string is a
+         subsequence of the second string, but returns False otherwise."""
+    if not a1:
+        return True
+    lp = b1.find(a1[0])
+    if lp == -1:
+        return False
+    else:
+        return is_subsequence(a1[1:], b1[lp+1:])
+
+
+
+
