3be09aa20371c018975c544076aade3fd5b7d6f0
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-19 14:31:28 -0700

    update

--- list_max.py
+++ list_max.py
@@ -6,17 +6,11 @@
 
 def list_max(nums):
     """a recursive function that takes as its parameter a list of numbers and returns the maximum value in the list."""
-    length = len(nums)
+    if len(nums) == 1:
+        return nums[0]
+    else:
+        number = list_max(nums[1:])
+        return number if number > nums[0] else nums[0]
 
-    if length == 1:
-        return nums
-
-    elif nums[length-1] >= nums[length-2]:
-        del nums[length-2]
-
-    elif nums[length-1] <= nums[length-2]:
-        del nums[length-1]
-
-    return list_max(nums)
-
-
+#nums = [1,2,-65,3,-1,6,45,2000, 6, 0]
+#print(list_max(nums))
