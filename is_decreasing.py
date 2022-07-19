7e0679aa6a3066f7795364a5f17ab6e1ea4e3f75
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-18 18:23:38 -0700

    update

--- is_decreasing.py
+++ is_decreasing.py
@@ -9,10 +9,17 @@
 def is_decreasing(nums):
     """Recursive function that takes as its parameter a list of numbers. It should return True if the elements of
     the list are strictly decreasing but return False otherwise."""
-    if len(nums) < 2:
+    if len(nums) <=1 or (len(nums) == 2 and nums[0] >= nums[1]):
         return True
-    if nums[0] > nums[1]:
+
+    elif nums[0] == nums[1]:
         return False
-    return is_decreasing(nums[1:])
+
+    else:
+        if nums[0]>= nums[1]:
+            return is_decreasing(nums[1::])
+
+        else:
+            return False
 
 
