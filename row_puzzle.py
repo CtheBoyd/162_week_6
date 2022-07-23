94d3376394f0f44be0ca0bac94abf0cedbd5b546
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-23 00:36:07 -0700

    update

--- row_puzzle.py
+++ row_puzzle.py
@@ -5,39 +5,38 @@
 # if the puzzle is solvable for that row, but returns False otherwise.
 #
 
-
-
-def puzzle_solver(nums, list_pos, memo_list):
-    """Description: Recursive function that takes a list of integers (the row) as a parameter and returns True
-       if the puzzle is solvable for that row, but returns False otherwise."""
-
+def puzzle_solver(nums, list_pos, memo=None):
+    """ Recursive function that takes a list of integers (the row) as a parameter and returns True
+               if the puzzle is solvable for that row, but returns False otherwise."""
     pos_num = nums[list_pos]
+
+    if memo is None:
+        memo = []
 
     if list_pos == (len(nums) - 1):
         return True
     elif list_pos > (len(nums) - 1):
         return False
-    elif pos_num == 0:
-        return False
-    elif memo_list[list_pos] != 0:
+    elif list_pos in memo:
         return False
 
-    move_left = False
-    move_right = False
-    memo_list[list_pos] = 1
+    left = right = False
+    memo.append(list_pos)
 
-    if (0 <= (list_pos - pos_num)):       #move left is valid
-        move_left = puzzle_solver(nums, list_pos - pos_num, memo_list)
+    if (0 <= (list_pos - pos_num)):  # move left is valid
+        left = puzzle_solver(nums, list_pos - pos_num, memo)
 
-    if len(nums) > list_pos + pos_num:   #move right is valid
-        move_right = puzzle_solver(nums, list_pos + pos_num, memo_list)
+    if len(nums) > (list_pos + pos_num):  # move right is valid
+        right = puzzle_solver(nums, list_pos + pos_num, memo)
 
-    return move_right or move_left
+    return right or left
+
+
 
 def row_puzzle(nums):
+    """Function runs the game."""
+    return puzzle_solver(nums, 0)
 
-    memo_list = []
-    for list_pos in range(len(nums)):
-        memo_list.append(0)
-    return puzzle_solver(nums, 0, memo_list)
+
+
 
